fc57b50304eeddc8b5eb70186ab28e3679ad3a88
Jason Ku <jku@jasontku.com>
2018-10-26 20:54:01 -0700

    Add support for Japanese.

--- spec/calculate-diff-spec.js
+++ spec/calculate-diff-spec.js
@@ -26,6 +26,16 @@ describe("calculateDiff", () => {
       expectedDiff: 15300000,
     },
     {
+      desc: 'japanese spanning am-pm',
+      eventTime: '午前10:15～午後2:30',
+      expectedDiff: 15300000,
+    },
+    {
+      desc: 'japanese only pm starting on an even hour',
+      eventTime: '午後3:00～4:45',
+      expectedDiff: 6300000,
+    },
+    {
       desc: 'short events with a location with dash-numbers',
       eventTime: '10:30am, DUR-5-Hey',
       expectedDiff: NaN,
