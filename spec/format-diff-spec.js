0548229bcd694f70069c4096cff64aa0651f792b
Jason Ku <jku@jasontku.com>
2020-03-21 21:07:50 -0700

    Add support in formatDiff for decimalHours formatting (ie, "2.5 hours").

---
+++ spec/format-diff-spec.js
@@ -0,0 +1,43 @@
+describe("formatDiff", () => {
+  const testCases = [
+    {
+      diff: 15300000,
+      expectedHourMinuteFormat: '4h 15m',
+      expectedDecimalHourFormat: '4.25 hours',
+    },
+    {
+      diff: 4500000,
+      expectedHourMinuteFormat: '1h 15m',
+      expectedDecimalHourFormat: '1.25 hours',
+    },
+    {
+      diff: 9456789,
+      expectedHourMinuteFormat: '2h 37m',
+      expectedDecimalHourFormat: '2.62 hours',
+    },
+    {
+      diff: 123456789,
+      expectedHourMinuteFormat: '34h 17m',
+      expectedDecimalHourFormat: '34.29 hours',
+    },
+    {
+      diff: 9000000,
+      expectedHourMinuteFormat: '2h 30m',
+      expectedDecimalHourFormat: '2.5 hours',
+    },
+  ];
+
+  testCases.forEach((testCase) => {
+    it(`should format diff with default format for ${testCase.diff}`, () => {
+      expect(formatDiff(testCase.diff)).toEqual(testCase.expectedHourMinuteFormat);
+    });
+
+    it(`should format diff with hourMinutes for ${testCase.diff}`, () => {
+      expect(formatDiff(testCase.diff, 'hourMinutes')).toEqual(testCase.expectedHourMinuteFormat);
+    });
+
+    it(`should format diff with decimalHours for ${testCase.diff}`, () => {
+      expect(formatDiff(testCase.diff, 'decimalHours')).toEqual(testCase.expectedDecimalHourFormat);
+    });
+  });
+});
